718fdd5ae24ea4e3fa76c34f4568973dc77bed68
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-04-01 20:36:22 +0200

    move data

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,9 +35,9 @@ docker exec $CONTAINER_ID curl 'http://127.0.0.1:5984/other-db' -X PUT -H 'Conte
 # Add test data from file
 echo "Adding test data to rtest database"
 docker exec $CONTAINER_ID mkdir /testdata
-docker cp data/import.json $CONTAINER_ID:/testdata/
-docker exec $CONTAINER_ID ls -R /testdata/
-docker exec $CONTAINER_ID echo "test"
+docker cp inst/testdata/import.json $CONTAINER_ID:/testdata/
+#docker exec $CONTAINER_ID ls -R /testdata/
+#docker exec $CONTAINER_ID echo "test"
 docker exec $CONTAINER_ID curl -X POST -H "Content-Type: application/json" -d @/testdata/import.json 'http://127.0.0.1:5984/rtest/_bulk_docs'
 
 echo "CouchDB set up complete"
