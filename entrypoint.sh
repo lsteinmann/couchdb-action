2e0742564f1eaab2fac2b983e25524ca7fba37e7
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-04-02 16:19:41 +0200

    change config of couchdb, update testdata

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,23 +21,24 @@ wait_for_couchdb
 # Set up system databases
 echo "Setting up CouchDB system databases"
 
-docker exec $CONTAINER_ID curl -sS 'http://127.0.0.1:5984/_users' -X PUT -H 'Content-Type: application/json' --data '{"id":"_users","name":"_users"}' > /dev/null
+docker exec $CONTAINER_ID curl 'http://127.0.0.1:5984/_users' -X PUT -H 'Content-Type: application/json' --data '{"id":"_users","name":"_users"}' > /dev/null
 
-docker exec $CONTAINER_ID curl -sS 'http://127.0.0.1:5984/_global_changes' -X PUT -H 'Content-Type: application/json' --data '{"id":"_global_changes","name":"_global_changes"}' > /dev/null
+docker exec $CONTAINER_ID curl 'http://127.0.0.1:5984/_global_changes' -X PUT -H 'Content-Type: application/json' --data '{"id":"_global_changes","name":"_global_changes"}' > /dev/null
 
-docker exec $CONTAINER_ID curl -sS 'http://127.0.0.1:5984/_replicator' -X PUT -H 'Content-Type: application/json' --data '{"id":"_replicator","name":"_replicator"}' > /dev/null
+docker exec $CONTAINER_ID curl 'http://127.0.0.1:5984/_replicator' -X PUT -H 'Content-Type: application/json' --data '{"id":"_replicator","name":"_replicator"}' > /dev/null
 
 # Create rtest database
 echo "Creating rtest + other-db databases"
 docker exec $CONTAINER_ID curl 'http://127.0.0.1:5984/rtest' -X PUT -H 'Content-Type: application/json'
-docker exec $CONTAINER_ID curl 'http://127.0.0.1:5984/other-db' -X PUT -H 'Content-Type: application/json'
+docker exec $CONTAINER_ID curl 'http://127.0.0.1:5984/anderes-projekt' -X PUT -H 'Content-Type: application/json'
+docker exec $CONTAINER_ID curl 'http://127.0.0.1:5984/test' -X PUT -H 'Content-Type: application/json'
+
+docker exec $CONTAINER_ID curl -X PUT 'http://127.0.0.1:5984/_node/nonode@nohost/_config/mango/default_limit' -H "Content-Type: application/json" -d '"100"'
 
 # Add test data from file
 echo "Adding test data to rtest database"
 docker exec $CONTAINER_ID mkdir /testdata
 docker cp inst/testdata/import.json $CONTAINER_ID:/testdata/
-#docker exec $CONTAINER_ID ls -R /testdata/
-#docker exec $CONTAINER_ID echo "test"
 docker exec $CONTAINER_ID curl -X POST -H "Content-Type: application/json" -d @/testdata/import.json 'http://127.0.0.1:5984/rtest/_bulk_docs'
 
 # Add a user with pwd "hallo"
